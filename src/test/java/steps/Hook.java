db7a3583cacc2d560581db239030d54726759551
sandeepghodke <42477745+sandeepghodke@users.noreply.github.com>
2018-08-28 16:06:07 +0530

    Update Hook.java

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -29,7 +29,7 @@ public class Hook extends BaseUtil{
 
 
         //Chrome driver
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\sandeepgh\\Documents\\Learning\\Jenkins\\chromedriver_linux64\\chromedriver");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\sandeepgh\\Documents\\Learning\\Jenkins\\chromedriver_win32\\chromedriver.exe");
         base.Driver = new ChromeDriver();
     }
 
